6815f12de6a240cd8b75ea38f6fce101c477ff42
langonginc <langonginc@yeah.net>
2020-08-24 16:53:10 +0800

    Push

--- install.sh
+++ install.sh
@@ -17,8 +17,13 @@ make
 echo " >> Install"
 echo "    => Copy bin"
 cp ./QLoveblog /usr/bin/qloveblog
+echo "    => Copy Desktop file"
+cp ./qloveblog.desktop /usr/share/applications
 echo "    => Copy dir"
 mkdir -p /usr/share/qloveblog
 cp -r ./* /usr/share/qloveblog
+
+echo " >> Change mode"
+chmod 755 /usr/share/applications/qloveblog.desktop
 
 echo " :: Finish"
